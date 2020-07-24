968f9b1c34cdb54c8ac4809b5096f3dc22c7f669
sandy171717 <maksandy405@gmail.com>
2020-07-24 16:53:47 +0530

    done by sandeep

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UseHook from './components/UseHook';
 
 function App() {
   return (
     <div className="App">
     <h1>Sandeep Makode</h1>
+    
+
+    <UseHook/>
      
     </div>
   );
---
+++ hook/src/components/UseHook.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function UseHook() {
+    return (
+        <div>
+        
+
+        <h1>Hello Mansi</h1>
+            
+        </div>
+    )
+}
+
+export default UseHook
